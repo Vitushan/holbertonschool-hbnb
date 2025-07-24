fc406571a43c643762719bae8da830dcf15c832f
Vitushan <vitushan@outlook.fr>
2025-07-24 15:35:19 +0200

    fixing tasks

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -3,11 +3,16 @@ from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import config
+
+
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
 db = SQLAlchemy()
+
+
 
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
@@ -16,31 +21,37 @@ from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
 from app.api.v1.protected import api as protected_ns
 
+
 def create_app(config_class=config.DevelopmentConfig):
-    # Création de l'application
     app = Flask(__name__)
-        
     app.config.from_object(config_class)
+
     
-    # Initialisation des extensions
+    CORS(app)
+
+   
     bcrypt.init_app(app)
     jwt.init_app(app)
     db.init_app(app)
-    
-    # Configuration de l'API
+
+
     authorizations = {
         'apikey': {
             'type': 'apiKey',
             'in': 'header',
             'name': 'Authorization',
-            'description': "Type in the *'Value'* input box below: **'Bearer &lt;JWT&gt;'**, where JWT is the token"
+            'description': "Type in the *'Value'* input box below: **'Bearer <JWT>'**, where JWT is the token"
         }
     }
-    
-    # Initialiser l'API
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', authorizations=authorizations)
 
-    # Ajouter les namespaces pour l'API
+    api = Api(
+        app,
+        version='1.0',
+        title='HBnB API',
+        description='HBnB Application API',
+        authorizations=authorizations
+    )
+
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
